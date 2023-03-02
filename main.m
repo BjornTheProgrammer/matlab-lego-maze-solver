e1dd24660521e85b5dce87486f35e7c53e666534
Bjorn Beishline <75190918+BjornTheProgrammer@users.noreply.github.com>
2023-03-02 10:58:28 -0700

    keyboard control

--- main.m
+++ main.m
@@ -1,13 +1,46 @@
-v = brick.GetBattVoltage() ;
+global key;
+InitKeyboard();
+
+v = brick.GetBattVoltage();
 % brick.SetColorMode(4)
 
-brick.TouchPressed(1) 
-while brick.TouchPressed(1) == 0
- brick.MoveMotor('A', 100);
- dist = brick.UltrasonicDist(2);
- disp(dist); 
-end
+drivePowerMultiplier = 1;
 
+brick.TouchPressed(1);
+while brick.TouchPressed(1) == 0
+    pause(0.001);
+    switch key
+        case 'a'
+            if drivePowerMultiplier - .05 >= 0
+                drivePowerMultiplier = drivePowerMultiplier - .05;
+            end
+        case 'd'
+            if drivePowerMultiplier + .05 <= 1
+                drivePowerMultiplier = drivePowerMultiplier + .05;
+            end
+        case 'q'
+            break
+        case 'uparrow'
+            brick.MoveMotor('AB', 100);
+        case 'downarrow'
+            brick.MoveMotor('AB', -100);
+        case 'leftarrow'
+            brick.MoveMotor('A', 100);
+            brick.MoveMotor('B', -100);
+        case 'rightarrow'
+            brick.MoveMotor('A', -100);
+            brick.MoveMotor('B', 100);
+        case 'w'
+            brick.MoveMotor('C', 100);
+        case 's'
+            brick.MoveMotor('C', -100);
+        case 0
+            brick.MoveMotor('ABC', 0);
+    end
+    dist = brick.UltrasonicDist(2);
+    disp(dist); 
+end
+CloseKeyboard();
 brick.StopAllMotors();
 
 disp('Done!')
